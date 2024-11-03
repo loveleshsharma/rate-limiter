cf4293f263cad05fc0672d1841bc19916a2fc094
Lovelesh Sharma <lovelesh90@gmail.com>
2024-11-03 16:36:39 +0530

    Adds basic server setup using gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/loveleshsharma/rate-limiter/internal"
-	"github.com/loveleshsharma/rate-limiter/internal/algorithm"
+	"github.com/loveleshsharma/rate-limiter/routes"
 )
 
 func main() {
-
-	rtLimiter := internal.NewRateLimiter(algorithm.NewTokenBucket(3))
-	fmt.Println(rtLimiter.IsRequestAllowed())
-
+	routes.SetupRoutes()
 }
