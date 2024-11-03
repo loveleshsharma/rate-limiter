3f678a2b03aba24de4bed1529c4cd29f5eb8eb98
Lovelesh Sharma <lovelesh90@gmail.com>
2024-11-03 11:57:47 +0530

    Implements bare minimum TokenBucket

--- rate-limiter/algorithm/token_bucket_algorithm.go
+++ rate-limiter/algorithm/token_bucket_algorithm.go
@@ -1,4 +1,46 @@
 package algorithm
 
-type TokenBucketAlgorithm struct {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	mx sync.Mutex
+
+	bucketCount int
+	capacity    int
+}
+
+func (b *TokenBucket) IsRequestAllowed() bool {
+	b.mx.Lock()
+	defer b.mx.Unlock()
+
+	if b.bucketCount < b.capacity {
+		return true
+	}
+
+	return false
+}
+
+func NewTokenBucket(capacity int) *TokenBucket {
+	tokenBucket := TokenBucket{
+		capacity: capacity,
+	}
+	ticker := time.NewTicker(time.Second)
+
+	go func() {
+		for {
+			select {
+			case t := <-ticker.C:
+				fmt.Println("resetting bucketCount at: ", t)
+				tokenBucket.mx.Lock()
+				tokenBucket.bucketCount = 0
+				tokenBucket.mx.Unlock()
+			}
+		}
+	}()
+
+	return &tokenBucket
 }
